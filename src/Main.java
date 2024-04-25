c35e622bcbc7bf56f33c82a297bf5ea6b75a9f85
DemchaAV <demchaav@gmail.com>
2024-04-25 14:12:18 +0100

    Additional

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import table.Table;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import static  table.Table.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         String path = "C:/Users/Demch/OneDrive/Рабочий стол/English learning/";
